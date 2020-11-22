d4d0619f19f58d92deb7f02b042f35a48d896b23
bryanbyrd <36066043+bryanbyrd@users.noreply.github.com>
2020-11-22 12:41:16 -0800

    added 3 tests
    getIssuingStore(), getBalance(), and deduct() verify that the respective
    functions in the GiftCard class returns the expected values.

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,53 @@
+package csc131.junit;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class GiftCardTest {
+
+	@Test
+	public void getIssuingStore() {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore());
+		
+	}
+	
+	@Test
+	public void getBalance() {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+				
+		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
+	}
+	
+	@Test
+	public void deduct() {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String result;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		result = "Remaining Balance: " + String.format("%6.2f", 95.00);
+	
+		assertEquals("deduct(5.00)", result, card.deduct(5.0));
+	}
+	
+}
+
+
